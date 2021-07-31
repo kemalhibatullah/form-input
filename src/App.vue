268c7d2cc29ea9e18ea3757db52973d6d4ec6443
kemalhibatullah <mohkemal@mail.ugm.ac.id>
2021-07-31 08:43:20 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Form/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Form
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   background-color : #02044A;
 }
 </style>
